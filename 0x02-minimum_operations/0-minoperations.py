a15cc7189d98eb2d2ca3049362b9dd15bea98858
kan-215 <katamialvin369@gmail.com>
2024-10-17 22:42:03 +0300

    minimum operations

--- 0x02-minimum_operations/0-minoperations.py
+++ 0x02-minimum_operations/0-minoperations.py
@@ -1,25 +1,26 @@
 #!/usr/bin/python3
-""" Module for 0-minoperations"""
+""" 0-minoperations module"""
 
 
 def minOperations(n):
     """
     minOperations
-    Gets fewest # of operations needed to result in exactly n H characters
+    gets the min number of operations required to generate exactly n H characters
     """
     # all outputs should be at least 2 char: (min, Copy All => Paste)
     if (n < 2):
         return 0
-    ops, root = 0, 2
-    while root <= n:
+    operations =0
+    divisor = 2
+    while divisor <= n:
         # if n evenly divides by root
-        if n % root == 0:
+        if n % divisor == 0:
             # total even-divisions by root = total operations
-            ops += root
+            operations += divisor
             # set n to the remainder
-            n = n / root
+            n = n / divisor
             # reduce root to find remaining smaller vals that evenly-divide n
-            root -= 1
+            divisor -= 1
         # increment root until it evenly-divides n
-        root += 1
-    return ops
+        divisor += 1
+    return operations
