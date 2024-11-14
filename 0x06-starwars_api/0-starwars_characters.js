34d3958ba3cd157586d63ab9bfc37d7300bc637f
kan-215 <katamialvin369@gmail.com>
2024-11-14 22:13:58 +0300

    star wars

--- 0x06-starwars_api/0-starwars_characters.js
+++ 0x06-starwars_api/0-starwars_characters.js
@@ -1,17 +1,43 @@
-#!/usr/bin/node
+#!/usr/bin/env node
+const axios = require('axios');
 
-const request = require('request');
+// Validate the command-line arguments
+if (process.argv.length !== 3) {
+  console.log('Usage: ./0-starwars_characters.js <Movie ID>');
+  process.exit(1);
+}
 
-request('https://swapi-api.hbtn.io/api/films/' + process.argv[2], function (err, res, body) {
-  if (err) throw err;
-  const actors = JSON.parse(body).characters;
-  exactOrder(actors, 0);
-});
-const exactOrder = (actors, x) => {
-  if (x === actors.length) return;
-  request(actors[x], function (err, res, body) {
-    if (err) throw err;
-    console.log(JSON.parse(body).name);
-    exactOrder(actors, x + 1);
-  });
-};
+const movieId = process.argv[2];
+
+// Validate that the Movie ID is a number
+if (isNaN(movieId)) {
+  console.log('Movie ID must be a number');
+  process.exit(1);
+}
+
+const apiUrl = `https://swapi.dev/api/films/${movieId}/`;
+
+// Function to fetch and display character names
+async function fetchCharacters() {
+  try {
+    const response = await axios.get(apiUrl);
+    const movieData = response.data;
+
+    if (!movieData.characters) {
+      console.log('Invalid Movie ID');
+      process.exit(1);
+    }
+
+    for (const characterUrl of movieData.characters) {
+      const characterResponse = await axios.get(characterUrl);
+      const characterData = characterResponse.data;
+      console.log(characterData.name);
+    }
+  } catch (error) {
+    console.error('Error:', error.message);
+    process.exit(1);
+  }
+}
+
+// Call the function
+fetchCharacters();
