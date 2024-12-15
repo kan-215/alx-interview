ff24a67cde6c6d0674131dcdf89271bc827b0131
kan-215 <katamialvin369@gmail.com>
2024-12-16 00:32:00 +0300

    prime game

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -1,64 +1,42 @@
 #!/usr/bin/python3
+"""Determines the winner of a prime game"""
+
+
 def isWinner(x, nums):
+    """x - rounds
+    nums - numbers list
     """
-    Determines the winner of the prime game.
-
-    Args:
-        x: Number of rounds to be played.
-        nums: List containing the maximum number for each round.
-
-    Returns:
-        Name of the player with the most wins ("Maria" or "Ben").
-        If no winner can be determined, return None.
-    """
-    if not nums or x < 1:
+    if x <= 0 or nums is None:
+        return None
+    if x != len(nums):
         return None
 
-    # Precompute the prime numbers up to the maximum n in nums
-    max_num = max(nums)
-    primes = sieve_of_eratosthenes(max_num)
-    prime_count = [0] * (max_num + 1)
+    ben_win = 0
+    maria_win = 0
 
-    # Create a cumulative prime count array
-    for i in range(1, max_num + 1):
-        prime_count[i] = prime_count[i - 1] + (1 if primes[i] else 0)
+    a = [1 for x in range(sorted(nums)[-1] + 1)]
+    a[0], a[1] = 0, 0
+    for j in range(2, len(a)):
+        rm_multiples(a, j)
 
-    maria_wins = 0
-    ben_wins = 0
-
-    # Play each round
-    for n in nums:
-        # Check the number of primes up to n
-        if prime_count[n] % 2 == 1:
-            maria_wins += 1  # Maria wins if the count is odd
+    for j in nums:
+        if sum(a[0:j + 1]) % 2 == 0:
+            ben_win += 1
         else:
-            ben_wins += 1  # Ben wins if the count is even
-
-    # Determine the overall winner
-    if maria_wins > ben_wins:
-        return "Maria"
-    elif ben_wins > maria_wins:
+            maria_win += 1
+    if ben_win > maria_win:
         return "Ben"
-    else:
-        return None
+    if maria_win > ben_win:
+        return "Maria"
+    return None
 
 
-def sieve_of_eratosthenes(n):
+def rm_multiples(ls, x):
+    """removes multiple
+    of primes
     """
-    Generates a boolean list indicating prime numbers up to n.
-
-    Args:
-        n: The upper limit of the range to check for primes.
-
-    Returns:
-        A list where True represents a prime number.
-    """
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False  # 0 and 1 are not prime numbers
-
-    for i in range(2, int(n**0.5) + 1):
-        if primes[i]:
-            for multiple in range(i * i, n + 1, i):
-                primes[multiple] = False
-
-    return primes
+    for j in range(2, len(ls)):
+        try:
+            ls[j * x] = 0
+        except (ValueError, IndexError):
+            break
